54dc2f631b0f92f24cb81c482c693dde61e5237b
snigdhadeep <snigdhadeep93@gmail.com>
2016-10-14 02:20:10 -0700

    first

---
+++ app/src/main/java/com/example/rp/foodmenudetails/Beandetail.java
@@ -0,0 +1,56 @@
+package com.example.rp.foodmenudetails;
+
+/**
+ * Created by apple on 15/03/16.
+ *
+ * //********LISTVIEW************
+ */
+public class Beandetail {
+
+    private int image;
+    private String title;
+    private String description;
+    private String ratings;
+
+
+    public Beandetail(int image, String title, String description,String ratings) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+        this.ratings =ratings;
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(String ratings) {
+        this.ratings = ratings;
+    }
+}
